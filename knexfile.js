82ddfdece2c5f4d1318abac5e700410df21127de
slaguna17 <slaguna17@gmail.com>
2025-02-02 18:18:40 -0400

    Knex for aws correction

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,8 @@ module.exports = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
       },
       migrations: {
         tableName: 'knex_migrations',
@@ -15,6 +16,10 @@ module.exports = {
       },
       seeds: {
         directory: "./seeds"
+      },
+      pool: {
+        min: 2,
+        max: 10,
       }
     }
   };
